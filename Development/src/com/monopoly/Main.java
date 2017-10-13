35fb87a507db35774aa803d387a04ccc94f2ff32
Yusuf Kamil AK <yusufkamilak@icloud.com>
2017-10-13 23:43:32 +0300

    Slack integration test

--- Development/src/com/monopoly/Main.java
+++ Development/src/com/monopoly/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
-        System.out.println("Oh bee");
+        System.out.println("Hadi be bilgehann");
     }
 }
