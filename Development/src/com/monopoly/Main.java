3964e69a3c85cf549d0946492861dbe401f10298
Bilgehan NAL <bilgehanl.03@gmail.com>
2017-10-14 02:24:31 +0300

    Script

--- Development/src/com/monopoly/Main.java
+++ Development/src/com/monopoly/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Intel IJ Problemini çozmuş bulunmaktayım")
+        System.out.println("Script test")
     }
 }
